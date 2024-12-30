c3b936293b886813cec47fbc067cb8ee92aa6601
md-suhag <abdussalamsuhag104@gmail.com>
2024-12-30 21:26:27 +0600

    initial setup done

---
+++ index.js
@@ -0,0 +1,26 @@
+const app = require("./app");
+const connectDB = require("./config/db");
+
+const port = process.env.PORT || 5000;
+console.log(process.env.NODE_ENV.length);
+connectDB();
+
+app.listen(port, () => {
+  console.log(`server is running at http://localhost:${port}`);
+});
+
+//unhandled promise rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`shutting down the server for ${err.message}`);
+  console.log(`shutting down the server for unhandle promise rejection`);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+// Handling uncaught Exception
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`shutting down the server for  handling uncaught exception`);
+  process.exit(1);
+});
