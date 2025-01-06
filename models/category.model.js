4cc738fdf69a238380a544b1fecdc2d7bc8941a6
md-suhag <abdussalamsuhag104@gmail.com>
2025-01-06 09:12:56 +0600

    category and coupon model added

--- models/category.model.js
+++ models/category.model.js
@@ -1,6 +1,12 @@
 const { default: mongoose, Schema } = require("mongoose");
 
-const categorySchema = new mongoose.Schema({});
+const categorySchema = new mongoose.Schema(
+  {
+    name: String,
+    parentCategory: { type: Schema.Types.ObjectId, ref: "Category" },
+  },
+  { timestamps: true }
+);
 
 export const Category =
   mongoose.models.Category || mongoose.model("Category", categorySchema);
--- models/coupon.model.js
+++ models/coupon.model.js
@@ -1,6 +1,11 @@
-const { default: mongoose, Schema } = require("mongoose");
+const { default: mongoose } = require("mongoose");
 
-const couponSchema = new mongoose.Schema({});
+const couponSchema = new mongoose.Schema({
+  code: { type: String, required: true },
+  discount: { type: String, required: true },
+  expireDate: { type: Date, required: true },
+  isActive: Boolean,
+});
 
 export const Coupon =
   mongoose.models.Coupon || mongoose.model("Coupon", couponSchema);
