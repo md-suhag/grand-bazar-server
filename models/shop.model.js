42e709b73df248d36200dd0454fd99143631ad85
md-suhag <abdussalamsuhag104@gmail.com>
2025-01-06 08:21:03 +0600

    added shop model

--- models/shop.model.js
+++ models/shop.model.js
@@ -1,5 +1,27 @@
 const { default: mongoose } = require("mongoose");
 
-const shopSchema = new mongoose.Schema({});
+const shopSchema = new mongoose.Schema(
+  {
+    vendorId: { type: Schema.Types.ObjectId, ref: "Vendor", required: true },
+    logo: {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+    description: String,
+    products: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Product",
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 export const Shop = mongoose.models.Shop || mongoose.model("Shop", shopSchema);
